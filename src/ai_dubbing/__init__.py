3720d92eeff06dcc75614d9e0c4b70defa50f3e1
Henry Zhang <it@mail.henry070.org>
2026-02-03 01:20:11 +0800

    Add __init__.py with __all__ exports

---
+++ src/ai_dubbing/__init__.py
@@ -0,0 +1,15 @@
+"""
+AI Video Dubbing Warehouse - 模块包初始化
+"""
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "asr",
+    "llm_director", 
+    "mixer",
+    "separator",
+    "tts_engine",
+    "utils",
+    "device",
+]
